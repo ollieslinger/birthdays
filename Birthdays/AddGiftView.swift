e8ad571d27d4a166e144a933d0cd53f981ade73b
ollieslinger <126179895+ollieslinger@users.noreply.github.com>
2024-12-10 13:22:49 -0500

    Added present list view and add gift view.

---
+++ Birthdays/AddGiftView.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Birthdays
+//
+//  Created by Ollie Hunter on 10/12/2024.
+//
+
+import Foundation
---
+++ Birthdays/PresentListView.swift
@@ -0,0 +1,8 @@
+//
+//  PresentListView.swift
+//  Birthdays
+//
+//  Created by Ollie Hunter on 10/12/2024.
+//
+
+import Foundation
